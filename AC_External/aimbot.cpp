56d7cc7de5681788cdba61ec0a5e20c487c2063e
SpatenBlut <h4.input@gmail.com>
2026-01-17 01:30:05 +0100

    litle

--- AC_External/aimbot.cpp
+++ AC_External/aimbot.cpp
@@ -1,29 +1,52 @@
 #include "Aimbot.h"
 
+
 void Aimbot(HANDLE hProcess) {
 
-    // Hole die ViewAngles vom LocalPlayer
     ViewAngles localAngles = GetLocalPlayerViewAngles(hProcess);
-
-    // Hole die Position vom LocalPlayer
     Position localPos = GetLocalPlayerPosition(hProcess);
 
-    // Ausgabe LocalPlayer Info
     std::cout << "=== LocalPlayer ===" << std::endl;
     std::cout << "Pitch: " << localAngles.pitch << "  Yaw: " << localAngles.yaw << std::endl;
     std::cout << "Position - X: " << localPos.x << " Y: " << localPos.y << " Z: " << localPos.z << std::endl;
     std::cout << std::endl;
 
-    // Hole alle Enemies
+    // Get Enemies
     std::vector<Enemy> enemies = GetEntitys(hProcess);
-
     std::cout << "=== Enemies ===" << std::endl;
+
+    float closestDistance = FLT_MAX;
+    int closestEnemyIndex = -1;
+
     for (size_t i = 0; i < enemies.size(); i++) {
+		// calculate distance to local player
+        float dx = enemies[i].x - localPos.x;
+        float dy = enemies[i].y - localPos.y;
+        float dz = enemies[i].z - localPos.z;
+        float distance = sqrt(dx * dx + dy * dy + dz * dz);
+
+		// print enemy info
         std::cout << "Enemy " << i
             << ": X=" << enemies[i].x
             << " Y=" << enemies[i].y
             << " Z=" << enemies[i].z
+            << " | Distance: " << distance
             << std::endl;
+
+		// check for closest enemy
+        if (distance < closestDistance) {
+            closestDistance = distance;
+            closestEnemyIndex = i;
+        }
     }
 
+	// print closest enemy info
+    if (closestEnemyIndex != -1) {
+        std::cout << "\n=== Closest Enemy ===" << std::endl;
+        std::cout << "Index: " << closestEnemyIndex << std::endl;
+        std::cout << "Distance: " << closestDistance << std::endl;
+        std::cout << "Position: X=" << enemies[closestEnemyIndex].x
+            << " Y=" << enemies[closestEnemyIndex].y
+            << " Z=" << enemies[closestEnemyIndex].z << std::endl;
+    }
 }
--- AC_External/aimbot.h
+++ AC_External/aimbot.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <windows.h>
 #include <iostream>
+#include <cmath>   // für sqrt()
+#include <cfloat>  // für FLT_MAX
 #include "EntityList.h"
 #include "memory.h"
 #include "offset.h"
